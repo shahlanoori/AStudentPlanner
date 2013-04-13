cc62ecb60edd01db5ac12c7accf90b37a29297c6
Shahla Noori <shahla1361@gmail.com>
2013-04-13 18:59:17 -0400

    Create GradeLetter class
    A new class is created in order to keep all the Grade Letters in it and
    by adding the proper functions and properties in this class, access is
    given to the rest part of the application to use this class whenever
    needed. The grade letters were used as an array of string in the system
    and they were scattered in different places in the project. By having
    this separate class we’ll increase the reuse, modifiability and
    maintenance.

---
+++ SourceCode/Calendar/GradeLetter.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Planner
+{
+    public class GradeLetter
+    {
+        private string _code;
+        public static GradeLetter A = new GradeLetter("A");
+        public static GradeLetter A_Minus = new GradeLetter("A-");
+
+        public static GradeLetter B_Plus = new GradeLetter("B+");
+        public static GradeLetter B = new GradeLetter("B");
+        public static GradeLetter B_Minus = new GradeLetter("B-");
+
+
+        public static GradeLetter C_Plus = new GradeLetter("C+");
+        public static GradeLetter C = new GradeLetter("C");
+        public static GradeLetter C_Minus = new GradeLetter("C-");
+
+        public static GradeLetter D_Plus = new GradeLetter("D+");
+        public static GradeLetter D = new GradeLetter("D");
+        public static GradeLetter D_Minus = new GradeLetter("D-");
+
+
+        private static GradeLetter[] _values = { A, A_Minus, B_Plus, B, B_Minus, C_Plus, C, C_Minus, D_Plus, D, D_Minus };
+
+        private GradeLetter(string code)
+        {
+            _code = code;
+        }
+        public string getCode()
+        {
+            return _code;
+        }
+        public static GradeLetter code(int arg)
+        {
+            return _values[arg];
+        }
+        public override string ToString()
+        {
+            return getCode();
+        }
+    }
+}
